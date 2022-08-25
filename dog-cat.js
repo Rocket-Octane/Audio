f33458f747079ec3be5be7c83c10df019ffe1550
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-08-24 22:05:37 -0400

    Add files via upload

---
+++ dog-cat.js
@@ -0,0 +1,46 @@
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("dog-cat.png");
+}
+
+function setup(){
+    canvas = createCanvas(650, 430);
+    canvas.position(300, 200);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is initialized");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 650, 430);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            fill("red");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("status").innerHTML = "Objects Detected";
+            document.getElementById("num_of_obj_detected").innerHTML = "There are 3 big objects in this image and all 3 were detected.";
+        }
+    }
+}
